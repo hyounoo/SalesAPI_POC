1a66e7c71278eb751746ebf005ab62e24f625ac6
Hyounwoo <hyounoosung@gmail.com>
2017-11-26 18:12:48 +0900

    Admin Area added.
    Admin module for Account, Product, and Purchase added.

---
+++ SalesAPI_POC/Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SalesAPI_POC.Models;
+
+namespace SalesAPI_POC.Areas.Admin.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private SalesAPI_POCContext db = new SalesAPI_POCContext();
+
+        // GET: Admin/Accounts
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Accounts.ToListAsync());
+        }
+
+        // GET: Admin/Accounts/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = await db.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Accounts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,CreatedDate,ModifiedDate")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(account);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = await db.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,CreatedDate,ModifiedDate")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = await db.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Account account = await db.Accounts.FindAsync(id);
+            db.Accounts.Remove(account);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SalesAPI_POC/Areas/Admin/Controllers/PurchasesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SalesAPI_POC.Models;
+
+namespace SalesAPI_POC.Areas.Admin.Controllers
+{
+    public class PurchasesController : Controller
+    {
+        private SalesAPI_POCContext db = new SalesAPI_POCContext();
+
+        // GET: Admin/Purchases
+        public async Task<ActionResult> Index()
+        {
+            var purchases = db.Purchases.Include(p => p.Account);
+            return View(await purchases.ToListAsync());
+        }
+
+        // GET: Admin/Purchases/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // GET: Admin/Purchases/Create
+        public ActionResult Create()
+        {
+            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/Purchases/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,CreatedDate,ModifiedDate,AccountId")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Purchases.Add(purchase);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "Name", purchase.AccountId);
+            return View(purchase);
+        }
+
+        // GET: Admin/Purchases/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "Name", purchase.AccountId);
+            return View(purchase);
+        }
+
+        // POST: Admin/Purchases/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,CreatedDate,ModifiedDate,AccountId")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchase).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "Name", purchase.AccountId);
+            return View(purchase);
+        }
+
+        // GET: Admin/Purchases/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: Admin/Purchases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            db.Purchases.Remove(purchase);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
